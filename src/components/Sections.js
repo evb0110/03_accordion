e7776c80e9b93826ccdf5230f4cf5e83fc3cf4e0
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2019-08-03 21:30:27 +0300

    added uuid

--- src/components/Sections.js
+++ src/components/Sections.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import Section from './Section';
+import uuid from 'uuid/v1';
 
 export default function Sections({ data }) {
-  return data.map(datum => <Section datum={ datum } />);
+  return data.map(datum => <Section datum={datum} key={uuid()} />);
 }
